68c7540607f6eeb0005339b1684531073cc3cdf1
Laura Wu <leemaiwu@gmail.com>
2023-07-02 14:28:13 -0600

    Completed with split and reverse method with for loop

--- solution.js
+++ solution.js
@@ -1,3 +1,25 @@
 function solution(str, ending){
-
+    let strFlip = str.split('').reverse()
+    let endingFlip = ending.split('').reverse()
+    let result = false
+    console.log(strFlip, endingFlip)
+    if (endingFlip.length === 0) {
+        return true
+    } else {
+        for (let i = 0; i < endingFlip.length; i++) {
+            if (endingFlip[i] !== strFlip[i]) {
+                return false
+            } else if (endingFlip[i] === strFlip[i]) {
+                result = true
+            } else {
+                result = false
+            }
+        }
+    }
+    return result
 }
+
+console.log(solution('abc', 'bc')) // true
+console.log(solution('abc', 'd')) // false
+console.log(solution('sumo', 'omo')) // false
+console.log(solution(':-)', ':-(')) // false
